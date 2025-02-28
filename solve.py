f32118528565ca9c67ab0cd2db9e2bfc37299954
Matt Kelliher <kelliherma1@gmail.com>
2025-02-28 15:39:45 -0500

    Added outline for additional functions

--- solve.py
+++ solve.py
@@ -5,8 +5,9 @@ class Computer():
         self.game = game.Game(computer=True, hardmode=hardmode)
         self.legalWords = self.game.ReturnLegalWords()
         self.availibleWords = self.legalWords
+        self.rewards = {0 : 0, 1 : 1, 2 : 2, 3 : 3}
 
-    def DetermineOptimalGuess(self, progress=False):
+    def OptimalGuess(self, progress=False):
         values = {}
         n = 0
         for guess in self.availibleWords:
@@ -24,6 +25,8 @@ class Computer():
         values = dict(sorted(values.items(), reverse=True))
         #print({k: v for i, (k, v) in enumerate(values.items()) if i < 100})
         return values[next(iter(values))][0]
+
+        # TODO Recognize the fact that multiple words may be returned
 
     def ExpectedValue(self, guess) -> int:
         scatterplot = {}
@@ -44,16 +47,30 @@ class Computer():
     def GetScore(self, key) -> int:
         score = 0
         for char in key:
-            score += int(char)
+            score += self.rewards[int(char)]
         
         return score
 
-    def Sort(self) -> list:
+        # TODO 0 should represent not finding new information, while black may still reward as information. Current system does not take this into account yet
+
+    def TuneScores(self, record=True):
         pass
+
+        # TODO Methodically test different scenerios to find optimal weights
+
+    def OpenScores(self):
+        pass
+
+        # TODO Open previously found scores that are located in local text file
+
+    def RemoveIncorrect(self):
+        pass
+
+        # TODO Function to remove all the possible words that are no longer eleible given the guesses made
 
 
 if __name__ == "__main__":
     myTest = Computer()
     #print(myTest.ExpectedValue("CRANE"))
     #print(myTest.ExpectedValue("MAGMA"))
-    print(myTest.DetermineOptimalGuess(progress=True))
+    print(myTest.OptimalGuess(progress=True))
