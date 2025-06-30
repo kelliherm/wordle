d84148a64052235f2ded9c91834e7865a86f0e75
Matt Kelliher <kelliherma1@gmail.com>
2025-06-30 19:52:51 -0400

    Added ability to load words

--- wordle/board.py
+++ wordle/board.py
@@ -1,7 +1,7 @@
 import colorama
 
 class Board:
-    def __init__(self, number_of_guesses=6):
+    def __init__(self, number_of_guesses=6) -> None:
         colorama.init()
 
         self.number_of_guesses = number_of_guesses
@@ -17,7 +17,7 @@ class Board:
             "RESET" : colorama.Style.RESET_ALL,
         }
     
-    def draw_board(self):
+    def draw_board(self) -> None:
         for row_index in range(self.number_of_guesses):
             print(row_index + 1, end="  ")
             if self.board[row_index] != []:
@@ -25,13 +25,13 @@ class Board:
                     print(self.colors[character_dict["COL"]] + character_dict["CHAR"], end="")
             print(self.colors["RESET"])
     
-    def destroy_board(self):
+    def destroy_board(self) -> None:
         colorama.deinit()
 
-    def reset_board(self):
+    def reset_board(self) -> None:
         self.board = [[] for i in range(self.number_of_guesses)]
 
-    def update_board(self, guess_list, guess_num=None):
+    def update_board(self, guess_list, guess_num=None) -> None:
         if guess_num != None:
             self.board[guess_num - 1] = guess_list
         else:
--- wordle/game.py
+++ wordle/game.py
@@ -1,29 +1,44 @@
+import json
 import random
 
+import board
+
 class Game:
-    def __init__(self):
-        pass
+    def __init__(self) -> None:      
+        self.load_words()
+        self.setup_game()
 
-    def destroy_game(self):
-        pass
+    def destroy_game(self) -> None:
+        self.board.destroy_board()
 
-    def get_user_guess(self):
-        pass
+    def get_user_guess(self) -> str:
+        return input("What is the player's guess?  ").upper()
+
+        # TODO Add error handling to user guess system, check value word, etc.
 
     @staticmethod
-    def return_guess_complete(self, guess, hidden_word=None):
+    def return_guess_complete(self, guess, hidden_word=None) -> list:
         pass
 
-    def load_words(self):
+    def load_words(self) -> None:
+        words_file = open("wordle\\words.json", "r")  # TODO Figure out python filestructure system for loading words/folder usage
+        words = json.load(words_file)
+        self.nyt_words = words["nyt_legal"]
+        self.all_words = words["all_legal"]
+        words_file.close()
+
+    def setup_game(self) -> None:
+        self.board = board.Board()
+        self.guess_number = 1
+
+    def reset_game(self) -> None:
+        self.board.reset_board()
+        self.guess_number = 1
+
+    def play_game(self) -> None:
         pass
 
-    def setup_game(self):
-        pass
-
-    def reset_game(self):
-        pass
-
-    def play_game(self):
+    def display_game(self) -> None:
         pass
 
 if __name__ == "__main__":
